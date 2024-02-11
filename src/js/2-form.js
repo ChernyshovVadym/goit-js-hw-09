77ea96708bb6d08486e911da8961e1e7fd1dff34
ChernyshovVadym <vad14@i.ua>
2024-02-11 13:28:17 +0200

    add

---
+++ src/js/2-form.js
@@ -0,0 +1,36 @@
+const form = document.querySelector(`.feedback-form`)
+form.addEventListener("input", (e) => {
+    const newForm = {
+        email: form.elements.email.value.trim(),
+        message: form.elements.message.value.trim()
+    }
+    localStorage.setItem("feedback-form-state", JSON.stringify(newForm));
+    console.log(newForm);
+});
+function formInit() {
+    const data = JSON.parse(localStorage.getItem("feedback-form-state"));
+    console.log(data);
+    if (data === null) {
+        return
+    }
+    form.email.value = data.email
+    form.message.value = data.message
+}
+formInit()
+
+formInit.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    if (emailInit.value && messageInit.value) {
+        console.log({
+            email: emailInit.value.trim(),
+            message: messageInit.value.trim(),
+        });
+        localStorage.removeItem("feedback-form-state");
+        emailInnit.value = "";
+        messageInnit.value = "";
+    } else {
+        alert("All form fields must be filled in");
+    }
+}
